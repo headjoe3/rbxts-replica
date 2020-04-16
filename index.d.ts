25e3bf5e9b093a33db39d179bb96af56226c6101
headjoe3 <robertjtulley@gmail.com>
2020-04-15 22:27:34 -0600

    Initial draft

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,112 @@
+import '@rbxts/types'
+
 export = Replica
 export as namespace Replica
 
+declare class Signal<CallbackType extends (...args: any[]) => void> {
+    Connect(callback: CallbackType): RBXScriptConnection
+    Fire(...args: FunctionArguments<CallbackType>): void
+    Wait(): FunctionArguments<CallbackType>
+    Destroy(): void
+}
+
+type ReplicantIndexKey = string | number
+type ReplicaRegistryKey = string | number | Instance
+type ReplicaSerializedValue = [ReplicantIndexKey | undefined, string, unknown]
+type ReplicaConfig = {
+    ServerCanSet: boolean
+    ClientCanSet: boolean
+    SubscribeAll: boolean
+    Whitelist: Array<Player>
+    Blacklist: Array<Player>
+}
+type ReplicantContext = {
+    base: Replica.Replicant
+    keyPath: Array<string>
+    config: ReplicaConfig
+    active: boolean
+    registryKey: ReplicaRegistryKey
+}
+
 declare namespace Replica {
-    abstract class Replicant {
-        
+    abstract class Replicant<T extends {[index: string]: any} = {[index: string]: unknown}>{
+        static FromSerialized(serialized: ReplicaSerializedValue, partialConfig?: Partial<ReplicaConfig>, context?: ReplicantContext): unknown
+        /** Note: this is meant to be called by lua code, and does not interface well with TypeScript classes.
+         * Consider using roblox-ts typings for lua code when extending the Replicant base class. */
+        static extend<StaticType, MembersType>(): [StaticType, MembersType]
+        static SerialType: string
+        GetConfig(): ReplicaConfig
+        SetConfig(newConfig: ReplicaConfig): void
+        Get<K extends keyof T>(key: K): T[K]
+        Set<K extends keyof T>(key: K, value: T[K]): void
+        Predict<K extends keyof T>(key: K, value: T[K]): void
+        Serialize(atKey?: keyof T, forClient?: Player): ReplicaSerializedValue
+        Collate(cb: () => void): void
+        Local(cb: () => void): void
+        GetValueWillUpdateSignal(key: keyof T): Signal<(isLocal: boolean) => void>
+        GetValueOnUpdateSignal(key: keyof T): Signal<(isLocal: boolean) => void>
+        VisibleToClient(client: Player): boolean
+        VisibleToAllClients(): boolean
+        CanReplicate(): boolean
+        Inspect(maxDepth?: number): void
+        MergeSerialized(serialized: ReplicaSerializedValue): void
+        Destroy(): void
     }
 
-    function Register(key: string | number | Instance, replicant: Replica.Replicant): void
+    class Array<T extends {[index: number]: any} = {[index: number]: unknown}> extends Replicant<T> {
+        static SerialType: 'ArrayReplicant'
+        IndexOf(value: T[keyof T]): number
+        Ipairs(): [<K extends keyof T>(object, number) => [K, T[K]], T, 0]
+        Size(): number
+        Insert(value: T[keyof T]): void
+        Insert(index: number, value: T[keyof T]): void
+        Insert(indexOrValue: number | T[keyof T], value?: T[keyof T]): void
+        Remove(index: number): void
+        Push(value: T[keyof T]): void
+        Pop(): void
+    }
+
+    class Map<T extends {[index: string]: any} = {[index: string]: unknown}> extends Replicant<T> {
+        static SerialType: 'MapReplicant'
+        Pairs(): [<K extends keyof T>(object, number) => [K, T[K]], T]
+    }
+
+    class FactoredOr<T extends {[index: string]: true} = {[index: string]: true}> extends Replicant<T> {
+        static SerialType: 'FactoredOrReplicant'
+        Pairs(): [<K extends keyof T>(object, number) => [K, T[K]], T]
+        ResolveState(): boolean
+        Set<K extends keyof T>(key: K, value: boolean): void
+        Reset(): void
+        Toggle<K extends keyof T>(key: K): void
+    }
+
+    class FactoredNor<T extends {[index: string]: true} = {[index: string]: true}> extends Replicant<T> {
+        static SerialType: 'FactoredNorReplicant'
+        Pairs(): [<K extends keyof T>(object, number) => [K, T[K]], T]
+        ResolveState(): boolean
+        Set<K extends keyof T>(key: K, value: boolean): void
+        Reset(): void
+        Toggle<K extends keyof T>(key: K): void
+    }
+
+    class FactoredSum<T extends {[index: string]: number} = {[index: string]: number}> extends Replicant<T> {
+        static SerialType: 'FactoredSumReplicant'
+        Pairs(): [<K extends keyof T>(object, number) => [K, T[K]], T]
+        ResolveState(): number
+        Reset(): void
+    }
+
+    function Register(key: ReplicaRegistryKey, replicant: Replica.Replicant): void
+
+    function Unregister(key: ReplicaRegistryKey): void
+
+    function WaitForRegistered(key: ReplicaRegistryKey, timeout: number): Replicant
+    function WaitForRegistered(key: ReplicaRegistryKey, timeout: undefined): Replicant | undefined
+
+    function GetRegistered(key: ReplicaRegistryKey): Replicant | undefined
+
+    const Deserialize: typeof Replicant.FromSerialized
+    const ReplicantRegistered: Signal<(replicant: Replicant, key: ReplicaRegistryKey) => void>
+    const ReplicantWillRegister: Signal<(replicant: Replicant, key: ReplicaRegistryKey) => void>
+    const ReplicantUnregistered: Signal<(replicant: Replicant, key: ReplicaRegistryKey) => void>
 }
